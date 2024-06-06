164d90b6ca06af1f4d9ca8d59823db3c1e4d67c0
NCElectronics <164473874+NCElectronics@users.noreply.github.com>
2024-06-06 19:04:28 +0200

    d pad buttons poc

--- python_joystick.ino
+++ python_joystick.ino
@@ -1,11 +1,32 @@
 #define joyX A0
 #define joyY A1
- 
+
+#define upButton 8
+#define downButton 9
+#define leftButton 10
+#define rightButton 11
+
 void setup() {
+  pinMode(upButton, INPUT_PULLUP);
+  pinMode(downButton, INPUT_PULLUP);
+  pinMode(leftButton, INPUT_PULLUP);
+  pinMode(rightButton, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+
   Serial.begin(115200);
 }
- 
+
 void loop() {
+  int up = digitalRead(upButton);
+  int down = digitalRead(downButton);
+  int left = digitalRead(leftButton);
+  int right = digitalRead(rightButton);
+  if (up == LOW || down == LOW || left == LOW || right == LOW) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
   int x = map(analogRead(joyX), 0, 1023, -512, 512);
   int y = map(analogRead(joyY), 0, 1023, -512, 512);
 
